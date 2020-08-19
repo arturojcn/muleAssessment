d9112a0a4ac194251361dd8b2779747927f3938c
arturojcn <arturojcn8@gmail.com>
2020-08-19 02:40:17 -0300

    POJO class to map metaIndicators and indicator files csv

---
+++ src/main/java/com/challenge/mule/model/csv/MetaIndicator.java
@@ -0,0 +1,13 @@
+package com.challenge.mule.model.csv;
+
+import lombok.Data;
+
+
+@Data
+public class MetaIndicator {
+    private String indicatorCode;
+    private String indicatorName;
+    private String note;
+    private String organization;
+    private String none;
+}
