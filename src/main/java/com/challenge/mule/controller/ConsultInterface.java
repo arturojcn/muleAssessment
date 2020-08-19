9a32244c92749d6aab637fa90b819e84a4270305
arturojcn <arturojcn8@gmail.com>
2020-08-19 10:10:57 -0300

    custom error response

--- src/main/java/com/challenge/mule/controller/ConsultInterface.java
+++ src/main/java/com/challenge/mule/controller/ConsultInterface.java
@@ -1,5 +1,6 @@
 package com.challenge.mule.controller;
 
+import com.challenge.mule.model.dto.ExceptionResponseDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -15,12 +16,24 @@ public interface ConsultInterface {
 
     @GetMapping("/worldPopulation")
     @ApiOperation(value = "get world population", tags = {"Consult"})
-    @ApiResponses(value = @ApiResponse(code = 200, message = "Success", response = String.class))
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = String.class),
+            @ApiResponse(code = 400, message = "Bad request, adjust before retrying", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 404, message = "Not found, resource wasn't found", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ExceptionResponseDTO.class ),
+            @ApiResponse(code = 503, message = "Time out", response = ExceptionResponseDTO.class )
+    })
     String getWorldPopulation();
 
 
     @GetMapping("/gdp_ppp")
     @ApiOperation(value = "get Gross Domestic Product and Purchasing-Power-Parity", tags = {"Consult"})
-    @ApiResponses(value = @ApiResponse(code = 200, message = "Success", response = String.class))
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = String.class),
+            @ApiResponse(code = 400, message = "Bad request, adjust before retrying", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 404, message = "Not found, resource wasn't found", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 503, message = "Time out", response = ExceptionResponseDTO.class)
+    })
     String getGdpPpp();
 }
