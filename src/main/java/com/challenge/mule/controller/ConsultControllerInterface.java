3ca135d3dfb0e6b36a048956e38d6557e85605ee
ajcabrera <arturo.cabrera@bibamgroup.com>
2020-08-20 00:20:23 -0300

    change the name class

--- src/main/java/com/challenge/mule/controller/ConsultControllerInterface.java
+++ src/main/java/com/challenge/mule/controller/ConsultControllerInterface.java
@@ -1,6 +1,9 @@
 package com.challenge.mule.controller;
 
 import com.challenge.mule.model.dto.ExceptionResponseDTO;
+import com.challenge.mule.model.dto.IndicatorDTO;
+import com.challenge.mule.model.dto.SearchIndicatorParams;
+import com.challenge.mule.model.dto.SearchParams;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -12,28 +15,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/api/v1", consumes = { MediaType.APPLICATION_JSON_VALUE },
         produces = { MediaType.APPLICATION_JSON_VALUE })
 @Api(value = "ConsultData", tags = { "Consult" })
-public interface ConsultInterface {
+public interface ConsultControllerInterface {
 
     @GetMapping("/worldPopulation")
-    @ApiOperation(value = "get world population", tags = {"Consult"})
+    @ApiOperation(value = "This endpoint get the top 20 countries by population growth from 2012 to 2019 Year-to-Year", tags = {"Consult"})
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success", response = String.class),
-            @ApiResponse(code = 400, message = "Bad request, adjust before retrying", response = ExceptionResponseDTO.class),
-            @ApiResponse(code = 404, message = "Not found, resource wasn't found", response = ExceptionResponseDTO.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ExceptionResponseDTO.class ),
-            @ApiResponse(code = 503, message = "Time out", response = ExceptionResponseDTO.class )
-    })
-    String getWorldPopulation();
-
-
-    @GetMapping("/gdp_ppp")
-    @ApiOperation(value = "get Gross Domestic Product and Purchasing-Power-Parity", tags = {"Consult"})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success", response = String.class),
+            @ApiResponse(code = 200, message = "Success", response = IndicatorDTO.class),
             @ApiResponse(code = 400, message = "Bad request, adjust before retrying", response = ExceptionResponseDTO.class),
             @ApiResponse(code = 404, message = "Not found, resource wasn't found", response = ExceptionResponseDTO.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = ExceptionResponseDTO.class),
             @ApiResponse(code = 503, message = "Time out", response = ExceptionResponseDTO.class)
     })
-    String getGdpPpp();
+    IndicatorDTO getWorldPopulationGrowth(SearchParams params);
+
+
+    @GetMapping("/indicators")
+    @ApiOperation(value = "This endpoint get by default the top 5 countries by GDP growth Indicator " +
+            "from the top 20 countries by population growth from 2012 to 2019 Year-to-Year", tags = {"Consult"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = IndicatorDTO.class),
+            @ApiResponse(code = 400, message = "Bad request, adjust before retrying", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 404, message = "Not found, resource wasn't found", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ExceptionResponseDTO.class),
+            @ApiResponse(code = 503, message = "Time out", response = ExceptionResponseDTO.class)
+    })
+    IndicatorDTO getIndicatorGrowth(SearchIndicatorParams params);
 }
